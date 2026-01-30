4471e199eb538636af2fa0ed013c01efec115af1
Nilesh Pawar <nnnpawar7+git@gmail.com>
2026-01-30 14:16:51 +0530

    Fix port number

--- main.py
+++ main.py
@@ -5,4 +5,5 @@ app = create_app()
 print("-----", __name__)
 
 if __name__== "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
